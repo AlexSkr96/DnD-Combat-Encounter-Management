3034bdef48925f03341cff5cc911c5e4e7903df1
Alex <github.tacky872@passmail.net>
2025-02-25 20:13:26 +0300

    Implement Attack class

--- src/character.py
+++ src/character.py
@@ -1,20 +1,37 @@
-class Character():
-    def __init__(self):
-        self.__hp = None
-        self.__ac
-        self.__speed
-        self.__initiative
+class Character:
+    def __init__(
+        self,
+        name: str,
+        hp: int,
+        ac: int,
+        speed: int,
+        strength: int,
+        intelligence: int,
+        dexterity: int,
+        wisdow: int,
+        constitution: int,
+        charisma: int,
+    ):
+        self.__name = None
+
+        self.__max_hp = None
+        self.hp = None
+        self.__ac = None
+        self.__speed = None
+        self.__initiative = None
 
         #### Abilities ####
-        self.__strength
-        self.__intelligence
-        self.__dexterity
-        self.__wisdom
-        self.__constitution
-        self.__charisma
+        self.__strength = None
+        self.__intelligence = None
+        self.__dexterity = None
+        self.__wisdom = None
+        self.__constitution = None
+        self.__charisma = None
         #### #### #### ####
 
         self.__gear = [] # List of ?weapon? class objects
         self.__spells = [] # List of spell class objects
         self.__traits = [] # List of trait class objects
         self.__multiattack = 1 # Number of attacks per main action
+
+    def attack(self, target: Character):
--- src/damage_dealing.py
+++ src/damage_dealing.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from service import AbilityType
 from die import *
+from character import Character
 
 
 DamageType = Enum("Damage Type", [
@@ -11,7 +12,7 @@ DamageType = Enum("Damage Type", [
 ])
 
 class Damage:
-    def __init__(self, damage_dice, damage_type):
+    def __init__(self, damage_dice: DieRoll, damage_type: DamageType):
         self.__damage_type = damage_type
         self.__damage_dice = damage_dice
 
@@ -19,8 +20,20 @@ class Damage:
         return self.__damage_dice.roll(), self.__damage_type
 
 
+class Attack:
+    def __init__(self, ability: AbilityType):
+        self.__ability = ability
+        self.__damages = []
+
+    def add_damage(self, damage: Damage):
+        self.__damages.append(damage)
+
+    # def attack(self, target: Character):
+
+
+
 class Weapon:
-    def __init__(self, name, w_range = 0, reach = 0):
+    def __init__(self, name: str, w_range = 0, reach = 0):
     # Range in feets, implies a ranged attack. None if can't make ranged attacks.
     # self.__range = w_range
     # Reach in feets, implies a meele attack. None if can't make meele attacks.
